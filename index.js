8c7d6cbfb07ca1771afc587d43467636dde25d85
Jaime Alvarez <jrralvarez@gmail.com>
2021-04-09 18:30:25 -0500

    able to create a 2D array grid.

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
 'Use Strict'
-console.log('Hello World.');
+console.log('Hello World');
+
+let createGrid = (width, height) => {
+    let grid = [];
+    let row = [];
+    for (let i = 0; i < width; i++) {
+        for (let j = 0; j < height; j++) {
+            row.push(" w ");
+        }
+        grid.push(row);
+        row = [];
+    }
+    return (grid);
+}
+
+displayGrid = (grid) => {
+    for (var i = 0; i < grid.length; i++) {
+        console.log(grid[i].join(""));
+    }
+}
+
+let gameGrid = createGrid(20, 20);
+displayGrid(gameGrid);
